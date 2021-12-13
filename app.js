80f40adeeb7ba037d08e25eecd952c8b95b4d7c8
Tuomas-tumple1991 <75422844+Tuomas-tumple1991@users.noreply.github.com>
2021-12-13 22:42:30 +0200

    app.get('*') siirretty

--- app.js
+++ app.js
@@ -49,9 +49,7 @@ app.use((req , res, next) => {
 app.use(express.static(path.join(__dirname, 'build')));
 
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+
 
 // TÄMÄ ON PAKKO OLLA!!!
 app.use(cookieParser()) 
@@ -777,7 +775,9 @@ console.log(' Konnektoi customer polulta')
 
 
 */
-    
+    app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(` Kuuntelee porttia ${port}`)
