2d4f1d89875fed97b14b0f1bc0cc56d789b17f8b
Tuomas-tumple1991 <75422844+Tuomas-tumple1991@users.noreply.github.com>
2021-12-13 23:44:34 +0200

    Update app.js
    lisätty rivit 27-32

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ app.use(express.static('build'))
 
 
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 
 // LIITÄNNÄT FRONTENDIIN
 app.use(
