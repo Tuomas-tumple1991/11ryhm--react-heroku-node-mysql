c88ef3764085a25b1c6af7ecf3354d908fa7a26d
Tuomas-tumple1991 <75422844+Tuomas-tumple1991@users.noreply.github.com>
2021-12-14 11:19:41 +0200

    Update app.js
    muutoksiqa

--- app.js
+++ app.js
@@ -40,11 +40,12 @@ app.use(
 
 
 app.use((req , res, next) => {
-  res.setHeader('Access-Control-Allow-Origin: *')
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Methods', ['PATCH', 'POST', 'GET', 'DELETE', 'PUT']);
   res.setHeader('Access-Control-Allow-Headers', ['Content-Type']);
   res.setHeader('Access-Control-Expose-Headers', ['Content-Type']);
+  res.setHeader('Access-Control-Allow-Origin', "http://localhost:3000")
+
   next();
 });
 
